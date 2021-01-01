b08fb8772e6b053f07556e1ab92ef9ff31b8092a
Benjamin Udoh <benjaminudoh10@gmail.com>
2021-01-01 11:27:28 +0100

    [Ben] Setup base component for app
    Signed-off-by: Benjamin Udoh <benjaminudoh10@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-body {
+import styled from "styled-components";
+
+const Main = styled.div`
+  font-size: 16px;
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background-color: #2b2a29;
+  height: 100vh;
+  max-height: 100vh;
+  width: 100vw;
+  max-width: 100vw;
+`;
+
+function App() {
+  return (
+    <Main></Main>
+  );
 }
 
-code {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-    monospace;
-}
+export default App;
